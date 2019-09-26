c17d24d60d6a655151b82d653c445b1ca1e47de7
Gabriel Pickl <gabriel@Gabriels-MacBook-Pro.local>
2019-09-26 20:22:45 +0200

    Added ability to create subdirectories.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,41 @@ use chrono::Local;
 use directories::ProjectDirs;
 use std::error::Error;
 use std::fs::create_dir_all;
+use std::path::PathBuf;
+use std::string::ToString;
+use structopt::StructOpt;
+
+#[derive(Debug)]
+enum AppError {
+    FailedToParseProjectName,
+}
+
+impl ToString for AppError {
+    fn to_string(&self) -> String {
+        match self {
+            AppError::FailedToParseProjectName => String::from("Failed to parse project name"),
+        }
+    }
+}
+
+fn parse_project_name(src: &str) -> Result<PathBuf, AppError> {
+    let path = PathBuf::from(src);
+    if path.components().count() != 1 {
+        return Err(AppError::FailedToParseProjectName);
+    }
+
+    Ok(path)
+}
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(parse(try_from_str = parse_project_name))]
+    name: Option<PathBuf>,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let opts = Options::from_args();
+
     let dirs = ProjectDirs::from("", "peacememories", "mess")
         .ok_or("Could not find application directory")?;
 
@@ -13,9 +46,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let year = today.year();
     let week_num = today.iso_week().week();
 
-    let dir = base_dir
+    let mut dir = base_dir
         .join(format!("{}", year))
         .join(format!("{}", week_num));
+
+    if let Some(name) = opts.name {
+        dir = dir.join(name);
+    }
     if !dir.exists() {
         create_dir_all(&dir)?;
     }
